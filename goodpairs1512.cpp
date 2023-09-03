8072649c18d90bc48a691e5df7b7184ecb3fefe4
keshav tiwari <138362706+Keshavtiwari07@users.noreply.github.com>
2023-09-03 19:33:27 +0530

    goodpairs1512

---
+++ goodpairs1512.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int numIdenticalPairs(vector<int>& nums) {
+     long long int keshav=0;
+        
+        for(int i=0;i<nums.size()-1;i++){
+            for(int j=i+1;j<nums.size();j++){
+//Check the condition for each element
+            if(nums[i]==nums[j]){
+//If Condition fullfill then increase the counter
+                keshav++;
+            }}
+
+        }
+//Return Counter
+     return keshav;   
+       
+    }
+};
